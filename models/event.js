783f27a92ccff1167d9f2b6672c3506d00ab0f25
saraoriola <saraoriola@outlook.com>
2023-10-04 18:52:48 +0200

    Modelo Event hecho

--- models/event.js
+++ models/event.js
@@ -1,25 +1,22 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class Event extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      Event.hasMany(models.Booking, { foreignKey: 'eventId' }); 
     }
   }
+
   Event.init({
     title: DataTypes.STRING,
     date: DataTypes.DATE,
-    location: DataTypes.STRING
+    location: DataTypes.STRING,
   }, {
     sequelize,
     modelName: 'Event',
+    tableName: 'events', 
   });
+
   return Event;
 };
