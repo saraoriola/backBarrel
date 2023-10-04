6bad03f1166a9a5c96c8172693e23eac14a2efb5
saraoriola <saraoriola@outlook.com>
2023-10-04 18:08:30 +0200

    Inicializando proyecto con éxito

---
+++ src/config/config.js
@@ -0,0 +1,32 @@
+const process = require("process");
+const mysql2 = require("mysql2");
+
+const config = {
+    development: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+    },
+    test: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+        dialectModule: mysql2,
+    },
+    production: {
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        dialect: process.env.DB_DIALECT,
+    },
+};
+
+module.exports = config;
