15908ed49779d1c5b0952b3d2c2485de0ada32fd
saraoriola <saraoriola@outlook.com>
2023-10-05 07:46:57 +0200

    deleteEvent creado correctamente

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -55,6 +55,24 @@ const EventController = {
         }
       },
 
+      async deleteEvent(req, res) {
+        const { id } = req.params; 
+      
+        try {
+          const event = await Event.findByPk(id);
+      
+          if (!event) {
+            return res.status(404).json({ error: 'Evento no encontrado' });
+          }
+          await event.destroy();
+      
+          return res.status(200).json({ message: 'Evento eliminado correctamente' });
+        } catch (error) {
+          console.error('Error al eliminar el evento:', error);
+          return res.status(500).json({ error: 'Error al eliminar el evento' });
+        }
+      }
+
 };
 
 module.exports = EventController;
--- routes/events.js
+++ routes/events.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.post('/createEvent', EventController.createEvent);
 router.get('/getAllEvents', EventController.getAllEvents);
 router.put('/updateEvent/:id', EventController.updateEvent);
+router.delete('/deleteEvent/:id', EventController.deleteEvent);
 
 module.exports = router;
