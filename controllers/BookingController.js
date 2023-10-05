32ca44a933ed747d9652a2f7fe0ab352f1379f62
saraoriola <saraoriola@outlook.com>
2023-10-05 13:51:04 +0200

    deletedAt de Bookings hecho

--- controllers/BookingController.js
+++ controllers/BookingController.js
@@ -2,67 +2,67 @@ const { Booking } = require('../models/index');
 
 const BookingController = {
   async createBooking(req, res) {
-        try {
-          const userId = req.user.id;           
-          const eventId = req.params.eventId; 
-          const { description } = req.body;
+    try {
+      const userId = req.user.id;
+      const eventId = req.params.eventId;
+      const { description } = req.body;
 
-          const existingBooking = await Booking.findOne({
-            where: { userId, eventId },
-          });
+      const existingBooking = await Booking.findOne({
+        where: { userId, eventId },
+      });
 
-          if (existingBooking) {
-            return res.status(400).json({ error: 'Ya tienes una reserva para este evento' });
-          }
+      if (existingBooking) {
+        return res.status(400).json({ error: 'Ya tienes una reserva para este evento' });
+      }
 
-          const newBooking = await Booking.create({
-            status: 'pending',
-            EventId: eventId, 
-            UserId: userId,   
-            description: description,
-          });
-          
-          return res.status(201).json(newBooking);
-        } catch (error) {
-          console.error('Error al crear la reserva:', error);
-          return res.status(500).json({ error: 'Error al crear la reserva' });
-        }
+      const newBooking = await Booking.create({
+        status: 'pending',
+        EventId: eventId,
+        UserId: userId,
+        description: description,
+      });
+
+      return res.status(201).json(newBooking);
+    } catch (error) {
+      console.error('Error al crear la reserva:', error);
+      return res.status(500).json({ error: 'Error al crear la reserva' });
+    }
   },
 
   async getMyBookings(req, res) {
-      try {
-        const { userId } = req.params;
-    
-        const bookings = await Booking.findAll({
-          where: { userId },
-          attributes: { exclude: ['EventId', 'UserId'] },
-        });
-    
-        return res.status(200).json(bookings);
-      } catch (error) {
-        console.error('Error al obtener las reservas:', error);
-        return res.status(500).json({ error: 'Error al obtener las reservas' });
-      }
+    try {
+      const { userId } = req.params;
+
+      const bookings = await Booking.findAll({
+        where: { userId },
+        attributes: { exclude: ['EventId', 'UserId'] },
+      });
+
+      return res.status(200).json(bookings);
+    } catch (error) {
+      console.error('Error al obtener las reservas:', error);
+      return res.status(500).json({ error: 'Error al obtener las reservas' });
+    }
   },
 
   async getBookingById(req, res) {
-      try {
-          const { bookingId } = req.params;
+    try {
+      const { bookingId } = req.params;
 
-          const booking = await Booking.findOne({
-              where: { id: bookingId },
-              attributes: { exclude: ['EventId', 'UserId'] },
+      const booking = await Booking.findOne({
+        where: { id: bookingId },
+        attributes: { exclude: ['EventId', 'UserId'] },
+      });
 
-          });
-
-          if (!booking) {
-              return res.status(404).json({ error: 'Reserva no encontrada' });
-          }
-          return res.status(200).json(booking);
-      } catch (error) {
-          console.error('Error al obtener la reserva por ID:', error);
-          return res.status(500).json({ error: 'Error al obtener la reserva por ID' });
+      if (!booking) {
+        return res.status(404).json({ error: 'Reserva no encontrada' });
       }
+
+      return res.status(200).json(booking);
+    } catch (error) {
+      console.error('Error al obtener la reserva por ID:', error);
+      return res.status(500).json({ error: 'Error al obtener la reserva por ID' });
+    }
   },
 
   async updateBooking(req, res) {
@@ -73,38 +73,40 @@ const BookingController = {
       const booking = await Booking.findOne({
         where: { id: bookingId },
       });
-      
+
       if (!booking) {
         return res.status(404).json({ error: 'Reserva no encontrada' });
       }
-      
+
       booking.description = description;
-  
+
       await booking.save();
-  
+
       return res.status(200).json(booking);
     } catch (error) {
       console.error('Error al actualizar la reserva:', error);
       return res.status(500).json({ error: 'Error al actualizar la reserva' });
     }
   },
-  
+
   async deleteBooking(req, res) {
     try {
       const { bookingId } = req.params;
-
-      const booking = await Booking.findByPk(bookingId);
-
+  
+      const booking = await Booking.findOne({
+        where: { id: bookingId },
+      });
+  
       if (!booking) {
         return res.status(404).json({ error: 'Reserva no encontrada' });
       }
-
-      await booking.destroy();
-
-      return res.status(204).json({ message: 'Reserva eliminada con éxito' }); 
+  
+      await booking.destroy({ force: false });
+  
+      return res.status(204).json({ message: 'Reserva marcada como eliminada con éxito' });
     } catch (error) {
-      console.error('Error al eliminar la reserva:', error);
-      return res.status(500).json({ error: 'Error al eliminar la reserva' });
+      console.error('Error al marcar la reserva como eliminada:', error);
+      return res.status(500).json({ error: 'Error al marcar la reserva como eliminada' });
     }
   },
   
